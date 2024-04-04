bb9054fc14a5f684f777d59c16e37f46f495a3ca
Vishvash011 <vishvashame011@gmail.com>
2024-04-04 12:28:27 +0530

    String, Numbers and Maths

---
+++ Chai or Code/Lec11Strings.js
@@ -0,0 +1,37 @@
+const name = "vishvash ";
+const repoCount = 8;
+// console.log(name + repoCount + " value");
+console.log(`Hello my name is ${name}and my repo count is ${repoCount}`);
+
+const playerName = new String("Rohit Sharma");
+console.log(playerName.length); // length of the string
+console.log(playerName[0]);
+console.log(playerName.charAt(1)); // get character at index 1
+// console.log(playerName.__proto__);
+console.log(playerName.concat(' is a good batsman'));
+console.log(playerName);
+console.log(playerName.toLowerCase());
+console.log(playerName.toUpperCase());
+console.log(playerName.toLocaleLowerCase());
+console.log(playerName.toLocaleUpperCase());
+console.log(playerName.indexOf('a',3)); //
+
+const newString = playerName.slice(3);
+console.log(newString);
+
+const anotherString = playerName.slice(-10,-5);
+console.log(anotherString);
+
+
+const oneString = "vishvash ame";
+console.log(oneString.trim());
+console.log(oneString.trimStart());
+console.log(oneString.trimEnd());
+
+console.log(oneString.replace("vishvash","pratham").trim());
+
+console.log(oneString.includes( 's' ));
+console.log(oneString.startsWith('i'));
+console.log(oneString.endsWith('h'));
+
+console.log(oneString.split('-'));
---
+++ Chai or Code/Lec12NumberAndMaths.js
@@ -0,0 +1,42 @@
+// const score = 400;
+// console.log(score);
+// const balance = new Number(100);
+// console.log(balance);
+// console.log(typeof balance);
+
+// console.log(balance.toString().length);
+// console.log(typeof balance);
+
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.8966;
+// console.log(otherNumber.toPrecision(5));
+
+// const hundreds = 1000000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++++++++ Maths ++++++++++++++++++++++
+
+
+console.log(Math);
+console.log(Math.abs(-17));
+console.log(Math.ceil(12.3));
+console.log(Math.floor(12.3));
+console.log(Math.round(12.3));
+
+const randomNum = Math.random();
+console.log(randomNum);
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor((Math.random() * 10) + 1));
+
+const max = 20;
+const min = 10;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+console.log(Math.max(1,2,3,4,5));
+console.log(Math.min(1,2,3,4,5));
+
+console.log(Math.pow(4,2));
+console.log(Math.sqrt(25));
